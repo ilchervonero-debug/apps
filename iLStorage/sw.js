21986142a522e6aea95fa18f151e499cce1a771f
ilchervonero-debug <ilchervonero@gmail.com>
2026-06-25 15:02:44 -0300

    PWA: manifest, service worker e iconos para cielorraso_dxf

---
+++ iLStorage/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'cielorraso-v1';
+const ASSETS = [
+  '/cielorraso_dxf.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://cdn.sheetjs.com/xlsx-0.20.1/package/dist/xlsx.full.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
