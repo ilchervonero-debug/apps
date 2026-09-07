d121aeb99d2b8ebde9f48efc1ae378c0b97abebf
ilchervonero-debug <ilchervonero@gmail.com>
2026-09-07 16:31:15 -0300

    Pizarra: agregar soporte offline

---
+++ chervo-web/apps/pizarra/sw.js
@@ -0,0 +1,5 @@
+const CACHE='pizarra-v1';
+const SHELL=['/apps/pizarra/','/apps/pizarra/index.html','/apps/pizarra/manifest.webmanifest','/apps/pizarra/icon.svg','/apps/pizarra/icon-192.png','/apps/pizarra/icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('pizarra-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.method!=='GET')return;const url=new URL(req.url);if(url.origin!==self.location.origin)return;if(req.mode==='navigate'){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put('/apps/pizarra/index.html',copy));return res}).catch(()=>caches.match('/apps/pizarra/index.html')));return}event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{if(res.ok)caches.open(CACHE).then(cache=>cache.put(req,res.clone()));return res})))})
